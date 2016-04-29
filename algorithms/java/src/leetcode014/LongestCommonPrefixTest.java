93191022f0ad760b7cd59143fc5e4d72fd9faa0c
VolterMiao <volter.miao@gmail.com>
2016-04-29 22:00:09 +0800

    java 014

---
+++ algorithms/java/src/leetcode014/LongestCommonPrefixTest.java
@@ -0,0 +1,19 @@
+package leetcode014;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LongestCommonPrefixTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
