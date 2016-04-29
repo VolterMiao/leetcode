ca3d4f2b3a97f0f79beb45582b034485f920d300
VolterMiao <volter.miao@gmail.com>
2016-04-29 21:04:29 +0800

    format code

--- java/src/leetcode104/MaximumDepthOfBinaryTree.java
+++ java/src/leetcode104/MaximumDepthOfBinaryTree.java
@@ -2,11 +2,11 @@ package leetcode104;
 
 public class MaximumDepthOfBinaryTree {
 	
-    public int maxDepth(TreeNode root) {
-        if(root == null)
+	public int maxDepth(TreeNode root) {
+		if(root == null)
 			return 0;
 		int dl = maxDepth(root.left);
 		int dr = maxDepth(root.right);
 		return 1 + ( dl > dr ? dl : dr );
-    }
+	}
 }
