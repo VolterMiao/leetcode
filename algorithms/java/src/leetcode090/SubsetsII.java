29ee60302d104675bffe4492086da8af741ec4ef
VolterMiao <volter.miao@gmail.com>
2016-04-30 19:45:23 +0800

    java 090 & 109

---
+++ algorithms/java/src/leetcode090/SubsetsII.java
@@ -0,0 +1,33 @@
+package leetcode090;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SubsetsII {
+	private List<List<Integer>> res = new ArrayList<List<Integer>>();
+	
+    public List<List<Integer>> subsetsWithDup(int[] S) {
+        boolean[] ax = new boolean[S.length];
+        Arrays.sort(S);
+        sub(0, S.length, S, ax);
+        return res;
+    }
+    
+    private void sub(int i, int n, int[] S, boolean[] ax){
+    	if(i >= n){
+    		List<Integer> l = new ArrayList<Integer>();
+    		for(int j=0; j<ax.length; j++)
+    			if(ax[j])
+    				l.add(S[j]);
+    		if(!res.contains(l))
+    			res.add(l);
+    	}
+    	else{
+    		ax[i] = true;
+    		sub(i+1, n, S, ax);
+    		ax[i] = false;
+    		sub(i+1, n, S, ax);
+    	}
+    }
+}
